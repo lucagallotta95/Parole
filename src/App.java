c5974b301f5258dee0a22f91add99f6c7bdaf0af
lucagallotta95 <lucagallotta95@gmail.com>
2024-12-17 13:41:36 +0100

    Passo 2

--- src/App.java
+++ src/App.java
@@ -8,27 +8,39 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
-        ArrayList<String> parole = new ArrayList<>(Arrays.asList("mare", "sole", "luce", "vento", "sale", "naso", "casa", "terra", "gas", "stella"));
-        
+        ArrayList<String> parole = new ArrayList<>(
+                Arrays.asList("mare", "sole", "luce", "vento", "sale", "naso", "casa", "terra", "gas", "stella"));
 
-        String parolaEstratta = parole.get(r.nextInt(parole.size()));
-        System.out.println("-----------------------------------------------------");
-        System.out.println("La parola estratta è: " + parolaEstratta);
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Inserisci una parola che contiene la parola estratta");
-        String parolaUtente = sc.nextLine();
+        for (int turno = 0; turno < 5; turno++) {
+            String parolaEstratta = parole.get(r.nextInt(parole.size()));
+            System.out.println("");
+            System.out.println("La parola estratta è: " + parolaEstratta);
+            System.out.println("");
+            System.out.println("Turno " + (turno + 1) + "- Giocatore 1");
+            System.out.println("Inserisci una parola che contiene la parola estratta");
+            String parolaGiocatore1 = sc.nextLine();
 
-        if (parolaUtente.indexOf(parolaEstratta)>=0)
-            System.out.println("Parola corretta");
+            if (parolaGiocatore1.indexOf(parolaEstratta) >= 0) {
+                System.out.println("Parola corretta");
+            } else {
+                System.out.println("Parola non corretta");
+            }
+            System.out.println("Turno " + (turno + 1) + "- Giocatore 2");
+            System.out.println("Inserisci una parola che contiene la parola estratta");
+            String parolaGiocatore2 = sc.nextLine();
 
-        else {
-            System.out.println("Parola non corretta");
+            if (parolaGiocatore2.indexOf(parolaEstratta) >= 0) {
+                System.out.println("Parola corretta");
+            } else {
+                System.out.println("Parola non corretta");
             }
 
-    
+            
 
+            
+        }
 
-
+        sc.close();
 
     }
 }
